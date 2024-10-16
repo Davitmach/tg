c30cdbe714b455d8e1f37a05aec0c3064f72b406
Davitmach <wvime30@gmail.com>
2024-10-16 10:26:21 +0400

    de

--- my-telegram-bot/index.js
+++ my-telegram-bot/index.js
@@ -2,22 +2,40 @@ const TelegramBot = require('node-telegram-bot-api');
 
 // Укажите токен, полученный от BotFather
 const token = '7725637136:AAGvJBDgs10Q-X7LVpxOrWXl7D4pl9gPUVU';
-
+const webAppUrl = 'https://davitmach.github.io/tg/my-telegram-bot/'
 // Создайте экземпляр бота
 const bot = new TelegramBot(token, { polling: true });
 
 // Реакция на команду /start
-bot.onText(/\/start/, (msg) => {
+bot.onText(/\/start/, async (msg) => {
   const chatId = msg.chat.id;
   console.log(msg,'msg');
   
   
   
-  bot.sendMessage(chatId, 'Привет! Я ваш бот!');
+  bot.sendMessage(chatId, `Привет ${msg.chat.first_name}! Я ваш бот!`);
+  await bot.sendMessage(chatId,'Щас кнопка будет', {
+    reply_markup: {
+      inline_keyboard:[
+        [{text:'зАполнить',web_app: {url:webAppUrl}}]
+      ]
+    }
+  })
 });
 
 // Ответ на любое сообщение
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   bot.sendMessage(chatId, `Вы сказали: ${msg.text}`);
+});
+
+
+bot.onText(/\/app/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'Вот ссылка на мое приложение: https://davitmach.github.io/tg/my-telegram-bot/');
+});
+
+bot.onText(/\/sendfile/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendDocument(chatId, './index.js');
 });
